b8f61c4c3a588ae70ece0d77b4b94681fab28ecf
Harshavardhan Sirdesai <harshsirdesai@gmail.com>
2026-08-16 00:04:54 +0530

    Implement reset functionality in data_ram module
    Added reset functionality to initialize memory on reset signal.

--- data_ram.v
+++ data_ram.v
@@ -1,28 +1,25 @@
 module data_ram(
-
-    input clk,we,
-  input [7:0] addr,
-     input [7:0] data_in,
-
+    input clk, we, rst,
+    input [7:0] addr,
+    input [7:0] data_in,
     output reg [7:0] data_out
-
 );
 
 reg [7:0] memory [0:255];
+integer i;
 
-always @(posedge clk)
-begin
-
-    if(we)
+always @(posedge clk) begin
+    if (rst) begin
+        for (i = 0; i < 256; i = i + 1) begin
+            memory[i] <= 8'd0;
+        end
+    end else if(we) begin
         memory[addr] <= data_in;
-
+    end
 end
 
-always @(*)
-begin
-
+always @(*) begin
     data_out = memory[addr];
-
 end
 
 endmodule
